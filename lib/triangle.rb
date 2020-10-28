851a7a215a4972e687ce8b0faa41cf586efbe06e
mccray austin <tubach75@hotmail.com>
2020-10-28 11:27:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
+  attr_accessor :side_1, :side_2, :side_3
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def tri
+    if (side_1 == side_2) && (side_2 == side_3)
+      :confirm
+    elsif (side_1 == side_2) && (side_2 < side_3 || side_2 > side_3)
+      :isosceles
+    elsif (side_1 != side_2) && (side_2 != side_3) && (side_1 != side_3)
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 
 end
